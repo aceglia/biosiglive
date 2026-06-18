Modify test for new dict_merger, force data_rate for param class, remove weighted moving average, fix tcp_interface

biosiglive/processing/data_processing.py
@@ -352,8 +352,6 @@ class RealTimeProcessing(GenericProcessing):
             True if apply normalization.
         moving_average_window : int
             Moving average window.
-        window_weights : list
-            Weights for the moving average.
 
         Returns
         -------
@@ -422,10 +420,7 @@ class RealTimeProcessing(GenericProcessing):
                     / quot
                 )
             elif moving_average:
-                # weights = window_weights if window_weights is not None else np.ones(ma_win)
-                # total_value_to_divide = sum(weights)
-                # average = (np.dot(emg_proc_tmp, weights) / total_value_to_divide)[:, None]
-                average = np.mean(emg_proc_tmp[:, -ma_win:], axis=1)[:, None]
+                average = np.median(emg_proc_tmp[:, -ma_win:], axis=1).reshape(-1, 1)
                 self.processed_data_buffer = np.append(self.processed_data_buffer[:, 1:], average / quot, axis=1)
         self.process_time.append(time.time() - tic)
         return self.processed_data_buffer.copy()